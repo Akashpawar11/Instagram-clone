502d6226213b38ed2a36bf5122e88794a07cab3c
Akash Pawar <akashpawar1103@gmail.com>
2022-09-16 11:15:56 +0530

    adjusted designing of search results page
    Signed-off-by: Akash Pawar <akashpawar1103@gmail.com>

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,9 +5,9 @@
             <div class="row">
                 <div class="input-group dark search-bar">
                     <router-link v-if="dataFetched" to="/"><i class="arrow-icon fa fa-arrow-left" style="font-size:24px"></i></router-link>
-                    <input type="text" v-model="username" class="form-control search-input dark" placeholder="Search">
+                    <input type="text" style="border-radius: 7px;" v-model="username" class="form-control search-input dark" placeholder="Search">
                     <button class="btn" @click="search" type="button">
-                        <span style="color:white" class="fa fa-search" aria-hidden="true"></span>
+                        <span style="color:white;padding: 10px 0 10px 10px;" class="fa fa-search" aria-hidden="true"></span>
                     </button>
                 </div>
             </div>
@@ -62,7 +62,19 @@
                 <div v-if="dataFetched" class="searchResults container-fluid p-0 m-0">
                     <div v-for="user in searchData" :key="user.id" class="user-thumbnail row">
                         <div class="user row d-flex">
-                            <router-link class="d-flex flex-row" :to=" `/public-posts/`+ user.user.pk ">
+                            <router-link v-if="user.user.is_private == false" class="d-flex flex-row" :to=" `/public-posts/`+ user.user.pk + '/public-acc'">
+                                <div class="imageDiv"><img class="imageSmall" src="../assets/users-avatar.jpg" alt="profile picture">
+                                </div>
+                                <div class="user-details d-flex flex-column">
+                                    <div>
+                                        <h5 style="color:antiquewhite; text-align: left; font-size: 1rem;">{{user.user.username}}</h5>
+                                    </div>
+                                    <div>
+                                        <h6 style="color:#646464;text-align: left; font-size: 1rem;">{{user.user.full_name}}</h6>
+                                    </div>
+                                </div>
+                            </router-link>
+                            <router-link v-if="user.user.is_private == true" class="d-flex flex-row" :to=" `/public-posts/`+ user.user.pk + '/pvt-acc'">
                                 <div class="imageDiv"><img class="imageSmall" src="../assets/users-avatar.jpg" alt="profile picture">
                                 </div>
                                 <div class="user-details d-flex flex-column">
@@ -107,15 +119,15 @@ export default {
     },
     methods: {
         async search() {
-            this.loading = true
             // API fetching in try block
             try {
+                this.loading = true
                 let username1 = this.username
                 const result = await axios.get(
                     'https://instagram47.p.rapidapi.com/search',
                     {
                         headers: {
-                            'X-RapidAPI-Key': 'd4903c298emshd5d047f07ca52bdp13d08ejsn0b2611e63d82',
+                            'X-RapidAPI-Key': '688eb0beeemsh8c7daafd5c3556ep15a70cjsn064a2a646b55',
                             'X-RapidAPI-Host': 'instagram47.p.rapidapi.com'
                         },
                         params: { search: username1 },
@@ -138,16 +150,27 @@ export default {
                     this.searchData = result.data.body.users
                     this.loading = false
                 }
+                this.loading = false
 
             } catch (error) {
-          //Catch block to show error/s
+                //Catch block to show error/s
                 console.log(error)
                 if (error.message == "Network Error") {
                     this.loading = false
-                    this.errMsg = "Network Error"
+                    this.Error = true
+                    this.errorMessage = "Network Error"
+                    console.log(this.errorMessage)
+                } else if (error.message == "Request failed with status code 429") {
+                    this.loading = false
+                    this.Error = true
+                    this.errorMessage = "Request failed with status code 429"
+                    console.log(this.errorMessage )
+                } else {
+                    this.loading = false
+                    this.Error = true
+                    this.errorMessage = error.message
+                    console.log(this.errorMessage)
                 }
-                this.err = true
-                this.errMsg = error.message
             }
         }
     }
@@ -157,7 +180,7 @@ export default {
 <style>
 body {
     padding: 0;
-    margin: 0;  
+    margin: 0;
     background-color: black;
 }
 
@@ -166,13 +189,16 @@ h4.error {
     font-family: Calibri;
     margin-top: 3rem;
 }
-.user-details{
-    padding: 7px 15px;
+
+.user-details {
+    padding: 8px 0 7px 15px;
 }
+
 .home-screen {
     height: 100vh;
 }
-.imageDiv{
+
+.imageDiv {
     display: flex;
     align-items: center;
     padding-left: 5px;
@@ -187,10 +213,22 @@ h4.error {
     background-color: black;
     margin: 6px 0 6px 6px;
     color: white;
+    border: 2px solid white;
+    font-family: Calibri;
+    border-radius: 6px;
+}
+
+.search-input:focus {
+    background-color: black;
+    margin: 6px 0 6px 6px;
+    color: white;
+    border: 1px solid white;
     font-family: Calibri;
 }
-.navbar{
+
+.navbar {
     border-bottom: 1px solid grey;
+    padding: 5px 0 5px 0;
 }
 
 .row {
